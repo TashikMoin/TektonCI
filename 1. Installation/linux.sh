1ea38491422329f1ce2a2b918613e8088176acdd
tashikmoin <tashikmoinsheikh@gmail.com>
2023-07-29 03:16:56 +0300

    updated

---
+++ 1. Installation/linux.sh
@@ -0,0 +1,17 @@
+# pipeline 
+kubectl apply --filename \
+https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+
+# triggers
+kubectl apply --filename \
+https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply --filename \
+https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+
+# tkn cli
+sudo apt update;sudo apt install -y gnupg
+sudo mkdir -p /etc/apt/keyrings/
+sudo mkdir -p /root/.gnupg
+sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/tektoncd.gpg --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
+echo "deb [signed-by=/etc/apt/keyrings/tektoncd.gpg] http://ppa.launchpad.net/tektoncd/cli/ubuntu eoan main"|sudo tee /etc/apt/sources.list.d/tektoncd-ubuntu-cli.list
+sudo apt update && sudo apt install -y tektoncd-cli
