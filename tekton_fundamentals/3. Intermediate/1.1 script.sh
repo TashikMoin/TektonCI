f99c44692e8974c908b87efcb40815f860b507bc
tashik.moin <tashik.moin@monese.com>
2023-08-14 11:41:22 +0300

    updated

--- tekton_fundamentals/3. Intermediate/1.1 script.sh
+++ tekton_fundamentals/3. Intermediate/1.1 script.sh
@@ -7,3 +7,6 @@ https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.6/git-c
 
 # kaniko
 kubectl apply -f https://api.hub.tekton.dev/v1/resource/tekton/task/kaniko/0.6/raw
+
+# git
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-cli/0.4/git-cli.yaml
